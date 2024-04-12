f0869f605dc1411ff4f4cdc138b84e7e77f0b811
ZellDell <wendell.hingosa@gmail.com>
2024-04-12 17:04:54 +0800

    switched to VPS MySQL database

--- Server-BackEnd/util/database.js
+++ Server-BackEnd/util/database.js
@@ -6,15 +6,33 @@
 
 // module.exports = sequelize;
 
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(
+//   "u478133511_Erkado",
+//   "u478133511_Erkado",
+//   "Erkado2024",
+//   {
+//     dialect: "mysql",
+//     host: "srv1158.hstgr.io",
+//   }
+// );
+
+// module.exports = sequelize;
+
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  "u478133511_Erkado",
-  "u478133511_Erkado",
-  "Erkado2024",
-  {
-    dialect: "mysql",
-    host: "srv1158.hstgr.io",
-  }
-);
+const sequelize = new Sequelize("erkadodb", "root", "Erkado2024", {
+  dialect: "mysql",
+  host: "93.127.198.109",
+  port: "3306",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = sequelize;
