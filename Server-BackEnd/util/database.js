a454a4626b08eafb8dfe41a60dc59de8cbe62b6e
ZellDell <wendell.hingosa@gmail.com>
2024-04-12 18:05:35 +0800

    VPS MySQL Database update 7

--- Server-BackEnd/util/database.js
+++ Server-BackEnd/util/database.js
@@ -22,7 +22,7 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("erkadodb", "root", "ERKADOAPP2024", {
   dialect: "mysql",
-  host: "localhost",
+  host: "127.0.0.1",
   port: "3306",
 });
 
